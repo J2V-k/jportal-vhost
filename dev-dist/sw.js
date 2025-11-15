7ce79560db3ac8e8c485953b89227cd2e74d6a0b
DhawalShankar <dhawalmannu@gmail.com>
2025-11-16 02:07:52 +0530

    Contributed an Elective Swap Portal

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3p4b34o5cq8"
+    "revision": "0.na30dpilf18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
